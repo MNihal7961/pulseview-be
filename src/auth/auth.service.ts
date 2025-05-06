1ddab83c9ad76f093c3091deda9fac275aa4808f
“MNihal7961 <mnihal7961@gmail.com>
2025-05-06 23:25:35 +0530

    feat(auth): implement user registration and authentication methods in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,148 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Injectable, Logger } from '@nestjs/common';
+import { SignUpUserDTO } from './dto/signup-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { comparePassword, hashPassword } from 'src/utils/hash';
+import { ObjectId } from 'mongodb';
+import { SignInUserDTO } from './dto/signin-user.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+	private readonly logger = new Logger(AuthService.name);
 
+	constructor(
+		@InjectRepository(User)
+		private usersRepository: Repository<User>,
+		private jwtService: JwtService,
+		private readonly configService: ConfigService,
+	) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+	async findOneByEmail(email: string) {
+		return await this.usersRepository.findOne({ where: { email } });
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+	async findOneById(id: string) {
+		const objectId = new ObjectId(id);
+		if (!objectId) {
+			return {
+				success: false,
+				message: 'Invalid user id',
+				data: null,
+			};
+		}
+		const user = await this.usersRepository.findOne({
+			where: { _id: objectId },
+		});
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+		return {
+			success: true,
+			message: 'User found',
+			data: user,
+		};
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+	async genarateJwtToken(user: User) {
+		const secret = this.configService.get<string>('AUTH_SECRET');
+		if (!secret) {
+			this.logger.error('JWT secret is not defined!');
+			throw new Error('JWT secret is not defined!');
+		}
+		const accessToken = this.jwtService.sign(
+			{
+				_id: user._id,
+				email: user.email,
+			},
+			{ secret, expiresIn: '1d' },
+		);
+
+		return accessToken;
+	}
+
+	async createNewUser(payload: SignUpUserDTO) {
+		try {
+			const existingUser = await this.findOneByEmail(payload.email);
+			if (existingUser) {
+				return {
+					success: false,
+					message: `User with email ${payload.email} already exists`,
+					data: null,
+				};
+			}
+
+			const user = new User();
+			user.firstName = payload.firstName;
+			user.lastName = payload.lastName;
+			user.email = payload.email;
+
+			const hashedPassword = await hashPassword(payload.password);
+			user.password = hashedPassword;
+
+			const newUser = await this.usersRepository.save(user);
+
+			return {
+				success: true,
+				message: 'User created successfully',
+				data: newUser,
+			};
+		} catch (error: any) {
+			this.logger.error('Error in creating new user', error);
+			return {
+				success: false,
+				message: error.message,
+				data: null,
+			};
+		}
+	}
+
+	async signin(payload: SignInUserDTO) {
+		try {
+			const user = await this.findOneByEmail(payload.email);
+			if (!user) {
+				return {
+					success: false,
+					message: `User with email ${payload.email} not found`,
+					data: null,
+				};
+			}
+
+			const isPasswordValid = await comparePassword(
+				payload.password,
+				user.password,
+			);
+			if (!isPasswordValid) {
+				return {
+					success: false,
+					message: 'Wrong password',
+					data: null,
+				};
+			}
+
+			const token = await this.genarateJwtToken(user);
+
+			if (!token) {
+				return {
+					success: false,
+					message: 'Error in generating token',
+					data: null,
+				};
+			}
+
+			return {
+				status: true,
+				message: 'Signin successful',
+				accessToken: token,
+				data: user,
+			};
+		} catch (error: any) {
+			this.logger.error('Error in signin', error);
+			return {
+				success: false,
+				message: error.message,
+				data: null,
+			};
+		}
+	}
 }
