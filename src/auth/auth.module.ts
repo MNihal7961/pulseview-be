57cbdff0189db153c2925ca4b25a93c40973675d
“MNihal7961 <mnihal7961@gmail.com>
2025-05-06 23:24:30 +0530

    feat(auth): integrate JwtModule and PassportModule into AuthModule for enhanced authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([User])],
+	imports: [TypeOrmModule.forFeature([User]), JwtModule, PassportModule],
 	controllers: [AuthController],
-	providers: [AuthService],
+	providers: [AuthService, JwtStrategy],
 	exports: [AuthService],
 })
 export class AuthModule {}
