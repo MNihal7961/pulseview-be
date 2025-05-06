f4b71237a4869ee3fb9d5eb40fec45e92bd8acbc
“MNihal7961 <mnihal7961@gmail.com>
2025-05-06 16:19:56 +0530

    feat(app): integrate TypeORM with MongoDB and configure global settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,32 @@ import { AuthModule } from './auth/auth.module';
 import { WeightEntriesModule } from './weight-entries/weight-entries.module';
 import { ShipmentsModule } from './shipments/shipments.module';
 import { MedicationsModule } from './medications/medications.module';
+import { User } from './auth/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [
-    AuthModule,
-    WeightEntriesModule,
-    ShipmentsModule,
-    MedicationsModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => ({
+				type: 'mongodb',
+				url: configService.get('DB_URL'),
+				entities: [User],
+				synchronize: true,
+				autoLoadEntities: true,
+			}),
+			inject: [ConfigService],
+		}),
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		AuthModule,
+		WeightEntriesModule,
+		ShipmentsModule,
+		MedicationsModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
