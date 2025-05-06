f2b1c850b3d0268199e8304ba4087795792aab0e
“MNihal7961 <mnihal7961@gmail.com>
2025-05-06 23:24:26 +0530

    refactor(auth): streamline authentication controller by removing unused methods and updating routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,34 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { SignUpUserDTO } from './dto/signup-user.dto';
+import { SignInUserDTO } from './dto/signin-user.dto';
+import { Public } from './public-auth-decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+	constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
+	@Public()
+	@Post('/signup')
+	async create(@Body() paylod: SignUpUserDTO) {
+		return await this.authService.createNewUser(paylod);
+	}
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
+	@Public()
+	@Post('/signin')
+	signin(@Body() payload: SignInUserDTO) {
+		return this.authService.signin(payload);
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
-  }
+	@Get('/user/:id')
+	findOne(@Param('id') id: string) {
+		return this.authService.findOneById(id);
+	}
 }
