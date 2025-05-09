6c6feb16ac66a80210f67fe3be9175395aaba664
“MNihal7961 <mnihal7961@gmail.com>
2025-05-10 00:14:07 +0530

    feat(medications): reorganize module structure to include TypeOrm imports for medication entity

--- src/medications/medications.module.ts
+++ src/medications/medications.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MedicationsService } from './medications.service';
 import { MedicationsController } from './medications.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Medication } from './entities/medication.entity';
 
 @Module({
-  controllers: [MedicationsController],
-  providers: [MedicationsService],
+	imports: [TypeOrmModule.forFeature([Medication])],
+	controllers: [MedicationsController],
+	providers: [MedicationsService],
+	exports: [MedicationsService],
 })
 export class MedicationsModule {}
