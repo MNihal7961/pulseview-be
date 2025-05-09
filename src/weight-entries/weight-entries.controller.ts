f855f2711ac8830f8511f04eb3a0facf889cb98b
“MNihal7961 <mnihal7961@gmail.com>
2025-05-10 00:24:56 +0530

    refactor(weight-entries): improve code readability and consistency in controller methods

--- src/weight-entries/weight-entries.controller.ts
+++ src/weight-entries/weight-entries.controller.ts
@@ -1,34 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	Query,
+} from '@nestjs/common';
 import { WeightEntriesService } from './weight-entries.service';
 import { CreateWeightEntryDto } from './dto/create-weight-entry.dto';
 import { UpdateWeightEntryDto } from './dto/update-weight-entry.dto';
 
 @Controller('weight-entries')
 export class WeightEntriesController {
-  constructor(private readonly weightEntriesService: WeightEntriesService) {}
+	constructor(private readonly weightEntriesService: WeightEntriesService) {}
 
-  @Post()
-  create(@Body() createWeightEntryDto: CreateWeightEntryDto) {
-    return this.weightEntriesService.create(createWeightEntryDto);
-  }
+	@Post()
+	async create(@Body() createWeightEntryDto: CreateWeightEntryDto) {
+		return await this.weightEntriesService.create(createWeightEntryDto);
+	}
 
-  @Get()
-  findAll() {
-    return this.weightEntriesService.findAll();
-  }
+	@Get()
+	async findAll(@Query('userId') userId: string) {
+		return await this.weightEntriesService.findAll(userId);
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.weightEntriesService.findOne(+id);
-  }
+	@Get(':id')
+	async findOne(@Param('id') id: string) {
+		return await this.weightEntriesService.findOne(id);
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWeightEntryDto: UpdateWeightEntryDto) {
-    return this.weightEntriesService.update(+id, updateWeightEntryDto);
-  }
+	@Patch(':id')
+	async update(
+		@Param('id') id: string,
+		@Body() updateWeightEntryDto: UpdateWeightEntryDto,
+	) {
+		return await this.weightEntriesService.update(id, updateWeightEntryDto);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.weightEntriesService.remove(+id);
-  }
+	@Delete(':id')
+	async remove(@Param('id') id: string) {
+		return await this.weightEntriesService.remove(id);
+	}
 }
