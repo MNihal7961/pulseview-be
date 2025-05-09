16a58477e4d89c1f31afb699131a214c718611fe
“MNihal7961 <mnihal7961@gmail.com>
2025-05-10 00:24:47 +0530

    feat(weight-entries): implement CRUD operations for weight entries with error handling

--- src/weight-entries/weight-entries.service.ts
+++ src/weight-entries/weight-entries.service.ts
@@ -1,26 +1,144 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateWeightEntryDto } from './dto/create-weight-entry.dto';
 import { UpdateWeightEntryDto } from './dto/update-weight-entry.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { WeightEntry } from './entities/weight-entry.entity';
+import { Repository } from 'typeorm';
+import { ObjectId } from 'mongodb';
 
 @Injectable()
 export class WeightEntriesService {
-  create(createWeightEntryDto: CreateWeightEntryDto) {
-    return 'This action adds a new weightEntry';
-  }
+	private readonly logger = new Logger(WeightEntriesService.name);
 
-  findAll() {
-    return `This action returns all weightEntries`;
-  }
+	constructor(
+		@InjectRepository(WeightEntry)
+		private weightEntriesRepository: Repository<WeightEntry>,
+	) {}
 
-  findOne(id: number) {
-    return `This action returns a #${id} weightEntry`;
-  }
+	async create(createWeightEntryDto: CreateWeightEntryDto) {
+		const { userId, weight } = createWeightEntryDto;
+		try {
+			const weightEntry = new WeightEntry();
+			weightEntry.userId = userId;
+			weightEntry.weight = weight;
+			await this.weightEntriesRepository.save(weightEntry);
 
-  update(id: number, updateWeightEntryDto: UpdateWeightEntryDto) {
-    return `This action updates a #${id} weightEntry`;
-  }
+			return {
+				success: true,
+				message: 'Weight entry created successfully',
+				data: weightEntry,
+			};
+		} catch (error: any) {
+			this.logger.error('Error in creating new goal', error);
+			return {
+				success: false,
+				message: error.message,
+				data: null,
+			};
+		}
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} weightEntry`;
-  }
+	async findAll(userId: string) {
+		try {
+			const weightEntries = await this.weightEntriesRepository.find({
+				where: { userId },
+			});
+			return {
+				success: true,
+				message: 'Weight entries fetched successfully',
+				data: weightEntries,
+			};
+		} catch (error: any) {
+			this.logger.error('Error in fetching weight entries', error);
+			return {
+				success: false,
+				message: error.message,
+				data: [],
+			};
+		}
+	}
+
+	async findOne(id: string) {
+		try {
+			const objectId = new ObjectId(id);
+			if (!objectId) {
+				return {
+					success: false,
+					message: 'Invalid weight entry id',
+					data: null,
+				};
+			}
+			const weightEntry = await this.weightEntriesRepository.findOne({
+				where: { _id: objectId },
+			});
+
+			return {
+				success: true,
+				message: 'Weight entry fetched successfully',
+				data: weightEntry,
+			};
+		} catch (error: any) {
+			this.logger.error('Error in fetching weight entry', error);
+			return {
+				success: false,
+				message: error.message,
+				data: null,
+			};
+		}
+	}
+
+	async update(id: string, updateWeightEntryDto: UpdateWeightEntryDto) {
+		try {
+			const objectId = new ObjectId(id);
+			if (!objectId) {
+				return {
+					success: false,
+					message: 'Invalid weight entry id',
+					data: null,
+				};
+			}
+			const weightEntry = await this.weightEntriesRepository.update(
+				objectId,
+				updateWeightEntryDto,
+			);
+			return {
+				success: true,
+				message: 'Weight entry updated successfully',
+				data: weightEntry,
+			};
+		} catch (error: any) {
+			this.logger.error('Error in updating weight entry', error);
+			return {
+				success: false,
+				message: error.message,
+				data: null,
+			};
+		}
+	}
+
+	async remove(id: string) {
+		try {
+			const objectId = new ObjectId(id);
+			if (!objectId) {
+				return {
+					success: false,
+					message: 'Invalid weight entry id',
+					data: null,
+				};
+			}
+			const weightEntry = await this.weightEntriesRepository.delete(objectId);
+			return {
+				success: true,
+				message: 'Weight entry deleted successfully',
+				data: weightEntry,
+			};
+		} catch (error: any) {
+			this.logger.error('Error in deleting weight entry', error);
+			return {
+				success: false,
+				message: error.message,
+				data: null,
+			};
+		}
+	}
 }
