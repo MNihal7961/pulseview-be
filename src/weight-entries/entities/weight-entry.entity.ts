3605c4d2a00c7b1f3f7aca788f10f72c673d4b84
“MNihal7961 <mnihal7961@gmail.com>
2025-05-09 23:54:41 +0530

    feat(weight-entry): implement WeightEntry entity with properties and decorators

--- src/weight-entries/entities/weight-entry.entity.ts
+++ src/weight-entries/entities/weight-entry.entity.ts
@@ -1,1 +1,28 @@
-export class WeightEntry {}
+import { Transform } from 'class-transformer';
+import {
+	Column,
+	CreateDateColumn,
+	Entity,
+	ObjectId,
+	ObjectIdColumn,
+	UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class WeightEntry {
+	@ObjectIdColumn()
+	@Transform((params) => params.obj._id.toString())
+	_id: ObjectId;
+
+	@Column()
+	userId: string;
+
+	@Column()
+	weight: number;
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@UpdateDateColumn()
+	updatedAt: Date;
+}
