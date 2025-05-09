c2938ce3254393f1ea776382d4509fcd653640e2
“MNihal7961 <mnihal7961@gmail.com>
2025-05-10 00:07:11 +0530

    feat(goal): define CreateGoalDto with properties for goal creation

--- src/goals/dto/create-goal.dto.ts
+++ src/goals/dto/create-goal.dto.ts
@@ -1,1 +1,8 @@
-export class CreateGoalDto {}
+export class CreateGoalDto {
+	userId: string;
+	type: 'weight' | 'height';
+	targetValue: number;
+	unit: 'kg' | 'cm';
+	startDate: Date;
+	endDate: Date;
+}
