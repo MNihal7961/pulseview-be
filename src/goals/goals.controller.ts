98c52469fe718f1912c37aaa57b5aa163344b426
“MNihal7961 <mnihal7961@gmail.com>
2025-05-10 00:07:28 +0530

    refactor(goals): improve code readability by reorganizing imports and adding async/await syntax

--- src/goals/goals.controller.ts
+++ src/goals/goals.controller.ts
@@ -1,34 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	Query,
+} from '@nestjs/common';
 import { GoalsService } from './goals.service';
 import { CreateGoalDto } from './dto/create-goal.dto';
 import { UpdateGoalDto } from './dto/update-goal.dto';
 
 @Controller('goals')
 export class GoalsController {
-  constructor(private readonly goalsService: GoalsService) {}
+	constructor(private readonly goalsService: GoalsService) {}
 
-  @Post()
-  create(@Body() createGoalDto: CreateGoalDto) {
-    return this.goalsService.create(createGoalDto);
-  }
+	@Post()
+	async create(@Body() createGoalDto: CreateGoalDto) {
+		return await this.goalsService.create(createGoalDto);
+	}
 
-  @Get()
-  findAll() {
-    return this.goalsService.findAll();
-  }
+	@Get()
+	async findAll(@Query('userId') userId: string) {
+		return await this.goalsService.findAll(userId);
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.goalsService.findOne(+id);
-  }
+	@Get(':id')
+	async findOne(@Param('id') id: string) {
+		return await this.goalsService.findOne(id);
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGoalDto: UpdateGoalDto) {
-    return this.goalsService.update(+id, updateGoalDto);
-  }
+	@Patch(':id')
+	async update(@Param('id') id: string, @Body() updateGoalDto: UpdateGoalDto) {
+		return await this.goalsService.update(id, updateGoalDto);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.goalsService.remove(+id);
-  }
+	@Delete(':id')
+	async remove(@Param('id') id: string) {
+		return await this.goalsService.remove(id);
+	}
 }
