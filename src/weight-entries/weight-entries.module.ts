b939e1afd392c995c06528e6f5014d3bab0def1d
“MNihal7961 <mnihal7961@gmail.com>
2025-05-10 00:24:52 +0530

    feat(weight-entries): reorganize module structure to include TypeOrm imports

--- src/weight-entries/weight-entries.module.ts
+++ src/weight-entries/weight-entries.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WeightEntriesService } from './weight-entries.service';
 import { WeightEntriesController } from './weight-entries.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WeightEntry } from './entities/weight-entry.entity';
 
 @Module({
-  controllers: [WeightEntriesController],
-  providers: [WeightEntriesService],
+	imports: [TypeOrmModule.forFeature([WeightEntry])],
+	controllers: [WeightEntriesController],
+	providers: [WeightEntriesService],
+	exports: [WeightEntriesService],
 })
 export class WeightEntriesModule {}
