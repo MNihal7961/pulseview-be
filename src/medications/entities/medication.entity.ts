9daf7cc7cc5e178bd726fb7d1c24e14c5972b8e8
“MNihal7961 <mnihal7961@gmail.com>
2025-05-09 23:54:37 +0530

    feat(medication): implement Medication entity with properties and decorators

--- src/medications/entities/medication.entity.ts
+++ src/medications/entities/medication.entity.ts
@@ -1,1 +1,37 @@
-export class Medication {}
+import { Transform } from 'class-transformer';
+import {
+	Column,
+	CreateDateColumn,
+	Entity,
+	ObjectId,
+	ObjectIdColumn,
+	UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Medication {
+	@ObjectIdColumn()
+	@Transform((params) => params.obj._id.toString())
+	_id: ObjectId;
+
+	@Column()
+	userId: string;
+
+	@Column()
+	type: string;
+
+	@Column()
+	dosage: string;
+
+	@Column()
+	startDate: Date;
+
+	@Column()
+	endDate: Date;
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@UpdateDateColumn()
+	updatedAt: Date;
+}
