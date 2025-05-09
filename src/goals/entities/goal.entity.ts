c2f9f756c82be4b5e178c177962c5964139a5523
“MNihal7961 <mnihal7961@gmail.com>
2025-05-09 23:54:33 +0530

    feat(goal): define Goal entity with properties and decorators

--- src/goals/entities/goal.entity.ts
+++ src/goals/entities/goal.entity.ts
@@ -1,1 +1,45 @@
-export class Goal {}
+import { Transform } from 'class-transformer';
+import {
+	Column,
+	CreateDateColumn,
+	Entity,
+	ObjectId,
+	ObjectIdColumn,
+	UpdateDateColumn,
+} from 'typeorm';
+
+export type GoalType = 'weight' | 'height';
+
+@Entity()
+export class Goal {
+	@ObjectIdColumn()
+	@Transform((params) => params.obj._id.toString())
+	_id: ObjectId;
+
+	@Column()
+	userId: string;
+
+	@Column()
+	type: GoalType;
+
+	@Column()
+	targetValue: number;
+
+	@Column()
+	unit: 'kg' | 'cm';
+
+	@Column()
+	startDate: Date;
+
+	@Column()
+	endDate: Date;
+
+	@Column()
+	status: 'active' | 'completed' | 'failed';
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@UpdateDateColumn()
+	updatedAt: Date;
+}
