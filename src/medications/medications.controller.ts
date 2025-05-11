ea393dee46e6da4d7c3e12c2f272f90b331ac1b9
“MNihal7961 <mnihal7961@gmail.com>
2025-05-11 23:07:49 +0530

    feat(medications): integrate MedicationLogsService for managing medication logs

--- src/medications/medications.controller.ts
+++ src/medications/medications.controller.ts
@@ -11,10 +11,16 @@ import {
 import { MedicationsService } from './medications.service';
 import { CreateMedicationDto } from './dto/create-medication.dto';
 import { UpdateMedicationDto } from './dto/update-medication.dto';
+import { MedicationLogsService } from './medication.logs.service';
+import { CreateMedicationLogsDto } from './dto/create-medication-logs.dto';
+import { UpdateMedicationLogsDto } from './dto/update-medication-logs.dto';
 
 @Controller('medications')
 export class MedicationsController {
-	constructor(private readonly medicationsService: MedicationsService) {}
+	constructor(
+		private readonly medicationsService: MedicationsService,
+		private readonly medicationLogsService: MedicationLogsService,
+	) {}
 
 	@Post()
 	async create(@Body() createMedicationDto: CreateMedicationDto) {
@@ -42,5 +48,37 @@ export class MedicationsController {
 	@Delete(':id')
 	async remove(@Param('id') id: string) {
 		return await this.medicationsService.remove(id);
+	}
+
+	// medication logs
+
+	@Post('/logs')
+	async createMedicationLogs(
+		@Body() createMedicationLogsDto: CreateMedicationLogsDto,
+	) {
+		return await this.medicationLogsService.create(createMedicationLogsDto);
+	}
+
+	@Get('/logs')
+	async findAllMedicationLogs(@Query('medicationId') medicationId: string) {
+		return await this.medicationLogsService.findAll(medicationId);
+	}
+
+	@Get('/logs/:id')
+	async findOneMedicationLogs(@Param('id') id: string) {
+		return await this.medicationLogsService.findOne(id);
+	}
+
+	@Patch('/logs/:id')
+	async updateMedicationLogs(
+		@Param('id') id: string,
+		@Body() updateMedicationLogsDto: UpdateMedicationLogsDto,
+	) {
+		return await this.medicationLogsService.update(id, updateMedicationLogsDto);
+	}
+
+	@Delete('/logs/:id')
+	async removeMedicationLogs(@Param('id') id: string) {
+		return await this.medicationLogsService.remove(id);
 	}
 }
