e69d07233481c09acadee235da8512c3ad1789d1
“MNihal7961 <mnihal7961@gmail.com>
2025-05-11 23:07:41 +0530

    feat(medications): include MedicationLogs in MedicationsModule imports and providers

--- src/medications/medications.module.ts
+++ src/medications/medications.module.ts
@@ -3,11 +3,13 @@ import { MedicationsService } from './medications.service';
 import { MedicationsController } from './medications.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Medication } from './entities/medication.entity';
+import { MedicatonLogs } from './entities/medication.logs.entity';
+import { MedicationLogsService } from './medication.logs.service';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Medication])],
+	imports: [TypeOrmModule.forFeature([Medication, MedicatonLogs])],
 	controllers: [MedicationsController],
-	providers: [MedicationsService],
-	exports: [MedicationsService],
+	providers: [MedicationsService, MedicationLogsService],
+	exports: [MedicationsService, MedicationLogsService],
 })
 export class MedicationsModule {}
