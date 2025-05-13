7dff3c19189c6c214b1d778705ef6f5a60119b8f
“MNihal7961 <mnihal7961@gmail.com>
2025-05-13 11:36:51 +0530

    fix(medications-module): correct import of MedicationLogs entity in medications module

--- src/medications/medications.module.ts
+++ src/medications/medications.module.ts
@@ -3,11 +3,11 @@ import { MedicationsService } from './medications.service';
 import { MedicationsController } from './medications.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Medication } from './entities/medication.entity';
-import { MedicatonLogs } from './entities/medication.logs.entity';
 import { MedicationLogsService } from './medication.logs.service';
+import { MedicationLogs } from './entities/medication.logs.entity';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Medication, MedicatonLogs])],
+	imports: [TypeOrmModule.forFeature([Medication, MedicationLogs])],
 	controllers: [MedicationsController],
 	providers: [MedicationsService, MedicationLogsService],
 	exports: [MedicationsService, MedicationLogsService],
